a43a583b1cf767cbc8fd3b7bd4fcf8ff501c03e8
Ricardo <ricardo.hemmel@hotmail.com>
2024-09-09 16:39:50 -0300

    Initial commit

---
+++ app/src/main/java/com/example/lojadehardware_alpha/Login.kt
@@ -0,0 +1,140 @@
+package com.example.lojadehardware_alpha
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.text.InputType
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+class Login : AppCompatActivity() {
+
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var togglePasswordVisibility: ImageView
+    private var isPasswordVisible: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Inicializa os elementos da interface
+        emailEditText = findViewById(R.id.userEmail)
+        passwordEditText = findViewById(R.id.userPassword)
+        togglePasswordVisibility = findViewById(R.id.togglePasswordVisibility)
+
+        // Configura o botão de visibilidade da senha
+        setupPasswordVisibilityToggle()
+
+        // Botão de login
+        val loginBtn: Button = findViewById(R.id.loginBtn)
+        loginBtn.setOnClickListener {
+            login()
+        }
+
+        // Link para a página de cadastro
+        val registerTxt: TextView = findViewById(R.id.registerTxt)
+        registerTxt.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun setupPasswordVisibilityToggle() {
+        // Estado inicial do ícone
+        togglePasswordVisibility.setImageResource(R.drawable.ic_visibility_off)
+
+        // Alterna a visibilidade da senha ao clicar no ícone
+        togglePasswordVisibility.setOnClickListener {
+            isPasswordVisible = !isPasswordVisible
+            if (isPasswordVisible) {
+                passwordEditText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                togglePasswordVisibility.setImageResource(R.drawable.ic_visibility_on)
+            } else {
+                passwordEditText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                togglePasswordVisibility.setImageResource(R.drawable.ic_visibility_off)
+            }
+            passwordEditText.setSelection(passwordEditText.text.length)
+        }
+    }
+
+    private fun login() {
+        val email = emailEditText.text.toString().trim()
+        val password = passwordEditText.text.toString().trim()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://43481d2e-b323-487d-8a32-4eeedb82e6d1-00-1hs63pwii6wo4.riker.replit.dev/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val apiService = retrofit.create(ApiService::class.java)
+
+        val call = apiService.login(email, password)
+        call.enqueue(object : Callback<List<LoginResponse>> {
+            override fun onResponse(
+                call: Call<List<LoginResponse>>,
+                response: Response<List<LoginResponse>>
+            ) {
+                if (response.isSuccessful && response.body() != null) {
+                    val loginResponses = response.body()!!
+                    if (loginResponses.isNotEmpty()) {
+                        // Utilização de Shared Preferences para armazenar o ID do usuário
+                        val sharedPreferences = getSharedPreferences("Dados", Context.MODE_PRIVATE)
+                        sharedPreferences.edit().apply {
+                            putInt("id", loginResponses[0].usuarioId)
+                            apply()
+                        }
+
+                        val intent = Intent(this@Login, Home::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Toast.makeText(this@Login, "Usuário ou senha inválidos", Toast.LENGTH_LONG).show()
+                    }
+                } else {
+                    Log.e("LoginActivity", "Login failed: HTTP error code: " + response.code() + " msg: " + response.message())
+                    Toast.makeText(this@Login, "Falha no login", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onFailure(call: Call<List<LoginResponse>>, t: Throwable) {
+                Log.e("LoginActivity", "onFailure: " + t.message)
+                Toast.makeText(this@Login, "Erro: ${t.message}", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    interface ApiService {
+        @GET("login")
+        fun login(
+            @Query("usuario") usuario: String,
+            @Query("senha") senha: String
+        ): Call<List<LoginResponse>>
+    }
+}
