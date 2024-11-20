d91807c98e6528558cd66639451e0bf60da6173d
lucasilmor <lucassilmor0@hotmail.com>
2024-11-20 04:03:02 -0300

    metodo auxiliar para encapsular metodos de busca e filtro

---
+++ app/src/main/java/com/example/lojadehardware_alpha/BaseSearchActivity.kt
@@ -0,0 +1,73 @@
+package com.example.lojadehardware_alpha
+
+import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lojadehardware_alpha.util.MenuFiltrosHelper
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+abstract class BaseSearchActivity : AppCompatActivity() {
+
+    protected lateinit var recyclerView: RecyclerView
+    protected lateinit var adapter: CustomAdapter
+    protected lateinit var searchView: SearchView
+    protected lateinit var apiService: ApiService
+    protected lateinit var searchResultsMessage: TextView
+    protected var termoBusca: String = ""
+    protected var filtroSelecionado: String? = null
+    protected lateinit var menuFiltrosHelper: MenuFiltrosHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        apiService = createRetrofitService("https://027c2e5f-4e20-4907-8ddb-002cce23454a-00-2bk0k8130zh8s.kirk.replit.dev/")
+    }
+
+    protected fun configurarSearchView(searchView: SearchView, onSearch: (String) -> Unit) {
+        searchView.setOnClickListener {
+            searchView.isIconified = false
+        }
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let {
+                    termoBusca = it
+                    onSearch(it)
+                    fecharTeclado()
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean = false
+        })
+    }
+
+    protected fun configurarButtonFiltros(button: Button, onFiltroSelecionado: () -> Unit) {
+        menuFiltrosHelper = MenuFiltrosHelper(this, button) { filtro ->
+            filtroSelecionado = filtro
+            onFiltroSelecionado()
+        }
+
+        button.setOnClickListener { view ->
+            menuFiltrosHelper.mostrarMenuFiltros(view)
+        }
+    }
+
+    protected fun fecharTeclado() {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        val view = this.currentFocus
+        imm.hideSoftInputFromWindow(view?.windowToken ?: searchView.windowToken, 0)
+    }
+
+    protected fun createRetrofitService(baseUrl: String): ApiService {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        return retrofit.create(ApiService::class.java)
+    }
+}
